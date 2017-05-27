5c1c90b37f52850dd9489f89d49b5c8aa06659f1
Juho Vepsalainen <bebraw@gmail.com>
2017-05-27 13:35:06 +0300

    chore: Port SideBody, navigation, footer
    Still need to look into `Page`.

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -5,35 +5,35 @@ import Icon from '../../assets/icon-square-small.svg';
 import CCBY from '../../assets/cc-by.png';
 import './footer-style';
 
-export default (props) => {
-  return (
-    <footer className="footer">
-      <Container className="footer__inner">
-        <section className="footer__left">
-          <Link className="footer__link" to="/guides/get-started">Get Started</Link>
-          <Link className="footer__link" to="/organization">Organization</Link>
-          <Link className="footer__link" to="/support">Support</Link>
-          <Link className="footer__link" to="/guides/why-webpack#comparison">Comparison</Link>
-        </section>
+const Footer = () => (
+  <footer className="footer">
+    <Container className="footer__inner">
+      <section className="footer__left">
+        <Link className="footer__link" to="/guides/get-started/">Get Started</Link>
+        <Link className="footer__link" to="/organization/">Organization</Link>
+        <Link className="footer__link" to="/support/">Support</Link>
+        <Link className="footer__link" to="/guides/why-webpack/#comparison">Comparison</Link>
+      </section>
 
-        <section className="footer__middle">
-          <Link to="/" className="footer__icon">
-            <img src={ Icon } />
-          </Link>
-        </section>
+      <section className="footer__middle">
+        <Link to="/" className="footer__icon">
+          <img src={ Icon } />
+        </Link>
+      </section>
 
-        <section className="footer__right">
-          <Link className="footer__link" to="/glossary">Glossary</Link>
-          <Link className="footer__link" to="/branding">Branding</Link>
-          <Link className="footer__link" to="//gitter.im/webpack/webpack">Gitter</Link>
-          <Link className="footer__link" to="https://github.com/webpack/webpack/releases">Changelog</Link>
-          <Link className="footer__link footer__license" to="/license">
-            <img
-              alt="Creative Commons License"
-              src={ CCBY } />
-          </Link>
-        </section>
-      </Container>
-    </footer>
-  );
-};
+      <section className="footer__right">
+        <Link className="footer__link" to="/glossary">Glossary</Link>
+        <Link className="footer__link" to="/branding/">Branding</Link>
+        <Link className="footer__link" to="//gitter.im/webpack/webpack">Gitter</Link>
+        <Link className="footer__link" to="https://github.com/webpack/webpack/releases">Changelog</Link>
+        <Link className="footer__link footer__license" to="/license">
+          <img
+            alt="Creative Commons License"
+            src={ CCBY } />
+        </Link>
+      </section>
+    </Container>
+  </footer>
+);
+
+export default Footer;
